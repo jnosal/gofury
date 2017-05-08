393017b751c4795c09efd9aa7b01e450fd1854fc
Jacek Nosal <jacek.nosal@outlook.com>
2017-05-08 20:22:45 +0200

    Added initial synchronisation mechanism

--- scraping/scraping.go
+++ scraping/scraping.go
@@ -11,6 +11,8 @@ import (
 )
 
 const HREF = "href"
+const LIMIT_CRAWL = 10
+const LIMIT_FAIL = 1
 
 func GetHref(t html.Token) (ok bool, href string) {
 	for _, a := range t.Attr {
@@ -65,13 +67,12 @@ type Runnable interface {
 type DefaultRunner struct {
 	finished    int
 	scrapers    []*Scraper
-	chDone      chan bool
+	chDone      chan *Scraper
 	chFoundUrls chan string
 }
 
 func (runner *DefaultRunner) FinishedIncr() {
 	runner.finished += 1
-
 }
 
 func (runner DefaultRunner) Done() bool {
@@ -83,7 +84,7 @@ func (runner *DefaultRunner) Run() {
 
 	for _, scraper := range runner.scrapers {
 		log.Printf("Starting: %s", scraper)
-		go scraper.Fetch(scraper.baseUrl)
+		go scraper.Start(scraper.baseUrl)
 	}
 
 	// main scraping loop
@@ -93,7 +94,8 @@ func (runner *DefaultRunner) Run() {
 			if !ok {
 				break
 			}
-		case _, ok := <-runner.chDone:
+		case scraper, ok := <-runner.chDone:
+			log.Printf("%s is done.", scraper)
 			runner.FinishedIncr()
 			if !ok {
 				break
@@ -120,7 +122,11 @@ func (runner *DefaultRunner) PushScraper(scrapers ...*Scraper) *DefaultRunner {
 }
 
 type Scraper struct {
+	crawled int
+	successful int
+	failed int
 	fetchMutex  *sync.Mutex
+	crawledMutex *sync.Mutex
 	domain      string
 	baseUrl     string
 	fetchedUrls map[string]bool
@@ -128,10 +134,37 @@ type Scraper struct {
 	extractor   *LinkExtractor
 }
 
+
+func (scraper *Scraper) IncrCounters(isSuccessful bool) {
+	scraper.crawledMutex.Lock()
+	scraper.crawled += 1
+	if (isSuccessful) {
+		scraper.successful += 1
+	} else {
+		scraper.failed += 1
+	}
+	scraper.crawledMutex.Unlock()
+}
+
+
 func (scraper *Scraper) MarkAsFetched(url string) {
 	scraper.fetchMutex.Lock()
 	scraper.fetchedUrls[url] = true
 	scraper.fetchMutex.Unlock()
+}
+
+
+func (scraper *Scraper) CheckIfShouldStop() (ok bool) {
+	scraper.crawledMutex.Lock()
+	if scraper.crawled == LIMIT_CRAWL {
+		log.Printf("Crawl limit exceeded: %s", scraper)
+		ok = true
+	} else if scraper.failed == LIMIT_FAIL {
+		log.Printf("Fail limit exceeeded: %s", scraper)
+		ok = true
+	}
+	scraper.crawledMutex.Unlock()
+	return
 }
 
 func (scraper *Scraper) CheckIfFetched(url string) (ok bool) {
@@ -152,42 +185,73 @@ func (scraper *Scraper) CheckUrl(sourceUrl string) (ok bool, url string) {
 	return
 }
 
-func (scraper *Scraper) Fetch(url string) {
+
+func (scraper *Scraper) RunExtractor(resp *http.Response) {
+	scraper.extractor.Extract(resp.Body, func(url string) {
+		ok, url := scraper.CheckUrl(url)
+
+		if ok {
+			go scraper.Fetch(url, true)
+		}
+
+		if scraper.CheckIfShouldStop() {
+			scraper.Stop()
+		}
+	})
+}
+
+
+func (scraper *Scraper) Stop() {
+	scraper.runner.chDone <- scraper
+}
+
+
+func (scraper *Scraper) Start(baseUrl string) {
+	resp, err :=  scraper.Fetch(baseUrl, false)
+
+	if err != nil {
+		log.Printf("Base url is corrupted stopping %s", scraper)
+		scraper.Stop()
+		return
+	}
+
+	scraper.RunExtractor(resp)
+
+	return
+}
+
+
+func (scraper *Scraper) Fetch(url string, extract bool) (resp *http.Response, err error) {
 	if ok := scraper.CheckIfFetched(url); ok {
 		return
 	}
 	scraper.MarkAsFetched(url)
 
 	log.Printf("Fetching: %s", url)
-	resp, err := http.Get(url)
+	resp, err = http.Get(url)
 
+	scraper.IncrCounters(err == nil)
 	if err != nil {
 		log.Printf("ERROR: Failed to crawl %s", url)
 		return
 	}
 
-	if scraper.extractor != nil {
-		scraper.extractor.Extract(resp.Body, func(url string) {
-			ok, url := scraper.CheckUrl(url)
-
-			if ok {
-				go scraper.Fetch(url)
-			}
-		})
+	if extract {
+		scraper.RunExtractor(resp)
 	}
 
 	return
 }
 
 func (scraper *Scraper) String() (result string) {
-	result = fmt.Sprintf("<Scraper: %s>", scraper.domain)
+	result = fmt.Sprintf("<Scraper: %s>. Crawled: %d, successful: %d failed: %d.", scraper.domain, scraper.crawled, scraper.successful, scraper.failed)
 	return
 }
 
 func NewRunner() (r *DefaultRunner) {
 	r = &DefaultRunner{
 		finished:    0,
-		chDone:      make(chan bool),
+		chDone:      make(chan *Scraper),
 		chFoundUrls: make(chan string),
 	}
 	return
@@ -195,9 +259,13 @@ func NewRunner() (r *DefaultRunner) {
 
 func NewScraper(domain string, url string) (s *Scraper) {
 	s = &Scraper{
+		crawled: 0,
+		successful: 0,
+		failed: 0,
 		domain:      domain,
 		baseUrl:     url,
 		fetchedUrls: make(map[string]bool),
+		crawledMutex: &sync.Mutex{},
 		fetchMutex:  &sync.Mutex{},
 		extractor:   &LinkExtractor{},
 	}
